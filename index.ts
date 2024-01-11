dd8f25ea47d0b5fb39b818b1b2df2da55811288e
Alex Oshchepkov <fastex93@gmail.com>
2024-01-11 21:24:50 +0300

    Правки после ревью

--- index.ts
+++ index.ts
@@ -5,42 +5,45 @@ import handlebars from 'handlebars';
 const app = fastify();
 
 app.register(fastifyView, {
-    engine: {
-        handlebars,
-    },
+  engine: {
+    handlebars,
+  },
 });
 
-app.get("/template", (req, reply) => {
-    reply.view("/templates/index.hbs", { title: 'Test' });
+app.get('/template', (req, reply) => {
+  reply.view('/templates/index.hbs', { title: 'Test' });
 });
 
-app.get('/api/users/', (request, reply) => {});
-app.get('/api/users/:userId/', (request, reply) => {});
-app.post('/api/users/', (request, reply) => {});
-app.put('/api/users/:userId/', (request, reply) => {});
+app.get('/api/users/', () => {});
+app.get('/api/users/:userId/', () => {});
+app.post('/api/users/', () => {});
+app.put('/api/users/:userId/', () => {});
 
-app.get('/api/courses/', (request, reply) => {});
-app.get('/api/courses/:courseId/', (request, reply) => {});
-app.post('/api/courses/', (request, reply) => {});
-app.put('/api/courses/:courseId/', (request, reply) => {});
+app.get('/api/courses/', () => {});
+app.get('/api/courses/:courseId/', () => {});
+app.post('/api/courses/', () => {});
+app.put('/api/courses/:courseId/', () => {});
 
-app.get('/api/lessons/:lessonId/', (request, reply) => {});
-app.post('/api/lessons/', (request, reply) => {});
-app.put('/api/lessons/:lessonId/', (request, reply) => {});
-app.delete('/api/lessons/:lessonId/', (request, reply) => {});
+app.get('/api/lessons/:lessonId/', () => {});
+app.post('/api/lessons/', () => {});
+app.put('/api/lessons/:lessonId/', () => {});
+app.delete('/api/lessons/:lessonId/', () => {});
 
-app.get('/api/comments/:lessonId/', (request, reply) => {});
-app.post('/api/comments/', (request, reply) => {});
+app.get('/api/comments/:lessonId/', () => {});
+app.post('/api/comments/', () => {});
 
-app.get('/api/roles/', (request, reply) => {});
-app.get('/api/roles/:roleId/', (request, reply) => {});
-app.post('/api/roles/', (request, reply) => {});
+app.get('/api/roles/', () => {});
+app.get('/api/roles/:roleId/', () => {});
+app.post('/api/roles/', () => {});
+
+app.post('/api/login', () => {});
+app.get('/api/logout', () => {});
 
 app.listen({ port: 8080 }, (err, address) => {
-    if (err) {
-        console.error(err)
-        process.exit(1)
-    }
-    
-    console.log(`Server listening at ${address}`)
-})
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log(`Server listening at ${address}`);
+});
